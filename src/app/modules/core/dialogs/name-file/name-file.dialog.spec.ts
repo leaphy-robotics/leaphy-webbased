1712dc3ab6178cf85c1240b6021268528643f15b
koen1711 <koenvurk1711@gmail.com>
2023-08-02 20:51:39 +0200

    Start on file saving

---
+++ src/app/modules/core/dialogs/name-file/name-file.dialog.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { NameFileDialog } from './name-file.dialog';
+
+describe('NameFileComponent', () => {
+  let component: NameFileDialog;
+  let fixture: ComponentFixture<NameFileDialog>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NameFileDialog ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NameFileDialog);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
