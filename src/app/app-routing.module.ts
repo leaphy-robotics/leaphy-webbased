67e6b457bd813f58d3458d3cdf58810c5546a3b3
koen1711 <za4266468@gmail.com>
2023-11-26 15:09:56 +0100

    chore: wrong route name (again)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
     { path: 'blocks', loadChildren: () => import('./modules/blockly-editor/blockly-editor.module').then(m => m.BlocklyEditorModule) },
-    { path: 'arduinoEditor', loadChildren: () => import('./modules/code-editor-arduino/code-editor.module').then(m => m.CodeEditorModule) },
+    { path: 'cppEditor', loadChildren: () => import('./modules/code-editor-arduino/code-editor.module').then(m => m.CodeEditorModule) },
     { path: 'pythonEditor', loadChildren: () => import('./modules/code-editor-python/code-editor-python.module').then(m => m.CodeEditorPythonModule) },
     { path: 'en/driverissues', loadChildren: () => import('./modules/driver-issues-english/driver-issues.module').then(m => m.DriverIssuesEnglishModule) },
     { path: 'nl/driverissues', loadChildren: () => import('./modules/driver-issues-dutch/driver-issues.module').then(m => m.DriverIssuesDutchModule) },
