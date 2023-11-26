71927816ab2d18effe040eacb7e83f0baef2ad43
koen1711 <za4266468@gmail.com>
2023-11-26 17:21:38 +0100

    chore: make everything not lazy loader

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,32 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CodeEditorPythonPage} from "./modules/code-editor-python/code-editor-python.page";
 import {CodeEditorPythonModule} from "./modules/code-editor-python/code-editor-python.module";
+import {DriverIssuesEnglishPage} from "./modules/driver-issues-english/driver-issues.page";
+import {DriverIssuesDutchModule} from "./modules/driver-issues-dutch/driver-issues.module";
+import {DriverIssuesDutchPage} from "./modules/driver-issues-dutch/driver-issues.page";
 
 
 const routes: Routes = [
-    { path: 'blocks', loadChildren: () => import('./modules/blockly-editor/blockly-editor.module').then(m => m.BlocklyEditorModule) },
-    { path: 'cppEditor', loadChildren: () => import('./modules/code-editor-cpp/code-editor-cpp.module').then(m => m.CodeEditorCppModule) },
+    { path: 'blocks', children: [{
+            path: '',
+            loadChildren: () => import('./modules/blockly-editor/blockly-editor.module').then(m => m.BlocklyEditorModule)
+        }]},
+    { path: 'cppEditor', children: [{
+            path: '',
+            loadChildren: () => import('./modules/code-editor-cpp/code-editor-cpp.module').then(m => m.CodeEditorCppModule)
+        }]},
     { path: 'pythonEditor', children: [{
             path: '',
             component: CodeEditorPythonPage
         }]},
-    { path: 'en/driverissues', loadChildren: () => import('./modules/driver-issues-english/driver-issues.module').then(m => m.DriverIssuesEnglishModule) },
-    { path: 'nl/driverissues', loadChildren: () => import('./modules/driver-issues-dutch/driver-issues.module').then(m => m.DriverIssuesDutchModule) },
+    { path: 'en', children: [{
+            path: 'driverissues',
+            component: DriverIssuesEnglishPage
+        }]},
+    { path: 'nl', children: [{
+            path: 'driverissues',
+            component: DriverIssuesDutchPage
+        }]}
 ];
 
 @NgModule({
