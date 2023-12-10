7330ab5d86ae6cbaf76909b82c787d79453d68b7
Jefta <jeftadegraaff@gmail.com>
2023-12-10 21:43:52 +0100

    Heb support voor het downloaden van een csv bestand toegevoegd

--- src/app/modules/shared/components/serial-output/serial-output.component.ts
+++ src/app/modules/shared/components/serial-output/serial-output.component.ts
@@ -13,6 +13,14 @@ import 'chartjs-adapter-moment';
 import { DialogState } from 'src/app/state/dialog.state';
 import { RobotWiredState } from 'src/app/state/robot.wired.state';
 import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import * as Papa from 'papaparse';
+import { Subscription } from 'rxjs';
+
+declare global {
+  interface Navigator {
+    msSaveBlob?: any;
+  }
+}
 
 @Component({
   selector: 'app-serial-output',
@@ -23,6 +31,9 @@ export class SerialOutputComponent implements AfterViewInit, OnInit {
 
   @ViewChildren('messages') messages: QueryList<any>;
   @ViewChild('content') content: ElementRef;
+
+  private serialDataSubscription: Subscription | undefined;
+  public serialDataAsJSON: any[] = [];
 
   constructor(
     public robotWiredState: RobotWiredState,
@@ -35,8 +46,51 @@ export class SerialOutputComponent implements AfterViewInit, OnInit {
     this.robotWiredState.serialData$.subscribe(() => {
       this.changeDetectorRef.detectChanges();
       this.scrollToBottom();
+      this.subscribeToSerialData();
     });
   }
+
+  ngOnDestroy(): void {
+    if (this.serialDataSubscription) {
+      this.serialDataSubscription.unsubscribe();
+    }
+  }
+
+  private subscribeToSerialData() {
+    interface SerialData {
+      time: Date;
+      data: string;
+    }
+
+    this.serialDataSubscription = this.robotWiredState.serialData$.subscribe((data: SerialData[]) => {
+      this.serialDataAsJSON = data.map(item => ({ time: item.time, data: item.data }));
+      // Additional logic if needed...
+    });
+  }
+
+  exportToCsv() {
+    var data = this.serialDataAsJSON
+    var filename = 'TEST_NAME.csv'
+
+    const csv = Papa.unparse(data, {
+      header: true
+    });
+
+    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) {
+      // For IE 10+
+      navigator.msSaveBlob(blob, filename);
+    } else {
+      const link = document.createElement('a');
+      if (link.download !== undefined) {
+        const url = URL.createObjectURL(blob);
+        link.setAttribute('href', url);
+        link.setAttribute('download', filename);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    }}
 
   ngAfterViewInit() {
     //https://stackoverflow.com/a/45655337/1056283
