80538c4436f454b5d26ea7942d0831d2585c871f
Ronald Moesbergen <rmoesbergen@users.noreply.github.com>
2024-02-02 14:41:03 +0100

    fix: don't auto-save to a file after loading

--- src/app/modules/blockly-editor/blockly-editor.page.ts
+++ src/app/modules/blockly-editor/blockly-editor.page.ts
@@ -1,11 +1,9 @@
 import {Component} from '@angular/core';
-import { BlocklyEditorState } from 'src/app/state/blockly-editor.state';
-import { DialogState } from 'src/app/state/dialog.state';
+import {BlocklyEditorState} from 'src/app/state/blockly-editor.state';
 import {BackendWiredEffects} from "../../effects/backend.wired.effects";
 import {CommonModule} from "@angular/common";
 import {SharedModule} from "../shared/shared.module";
 import {BlocklyEditorModule} from "./blockly-editor.module";
-import {PythonFile} from "../../domain/python-file.type";
 
 @Component({
     standalone: true,
@@ -22,23 +20,11 @@ import {PythonFile} from "../../domain/python-file.type";
 export class BlocklyEditorPage {
     constructor(
         public blocklyState: BlocklyEditorState,
-        public dialogState: DialogState,
         private backendWiredEffects: BackendWiredEffects,
         private blocklyEditorState: BlocklyEditorState
     ) {
         window.addEventListener("beforeunload", async (event) => {
-            if (blocklyState.getProjectFileHandle()) {
-                const file = this.blocklyState.getProjectFileHandle();
-                if (!(file instanceof PythonFile)) {
-                    const currentContent = await (await file.getFile()).text();
-                    if (currentContent == this.blocklyState.workspaceXml) {
-                        return;
-                    }
-                    this.backendWiredEffects.send('save-workspace', {data: this.blocklyEditorState.workspaceXml});
-                }
-            } else {
-                this.backendWiredEffects.send('save-workspace-temp', {data: this.blocklyEditorState.workspaceXml})
-            }
+            this.backendWiredEffects.send('save-workspace-temp', {data: this.blocklyEditorState.workspaceXml})
         });
     }
 }
--- src/app/modules/code-editor-python/code-editor-python.page.ts
+++ src/app/modules/code-editor-python/code-editor-python.page.ts
@@ -1,12 +1,9 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { CodeEditorState } from 'src/app/state/code-editor.state';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {CodeEditorState} from 'src/app/state/code-editor.state';
 import {BackendWiredEffects} from "../../effects/backend.wired.effects";
 import {CommonModule} from "@angular/common";
 import {SharedModule} from "../shared/shared.module";
 import {CoreModule} from "../core/core.module";
-import {BlocklyEditorState} from "../../state/blockly-editor.state";
-import {PythonFile} from "../../domain/python-file.type";
-import {PythonUploaderService} from "../../services/python-uploader/PythonUploader.service";
 
 
 @Component({
@@ -27,25 +24,13 @@ export class CodeEditorPythonPage implements AfterViewInit {
     constructor(
         private backendWiredEffects: BackendWiredEffects,
         private codeEditorState: CodeEditorState,
-        private blocklyState: BlocklyEditorState,
-        private pythonUploaderService: PythonUploaderService
     ) {}
 
     ngAfterViewInit(): void {
         this.codeEditorState.setAceElement(this.editor);
 
         window.addEventListener("beforeunload", async (event) => {
-            if (this.blocklyState.getProjectFileHandle()) {
-                // buy some time for the backend to save the file
-                const file = this.blocklyState.getProjectFileHandle();
-                if (file instanceof PythonFile) {
-                    this.pythonUploaderService.runFileSystemCommand('put', file.path, this.codeEditorState.getCode());
-                } else {
-                    this.backendWiredEffects.send('save-workspace', {data: this.codeEditorState.getCode()});
-                }
-            } else {
-                this.backendWiredEffects.send('save-workspace-temp', {data: this.codeEditorState.getCode()});
-            }
+            this.backendWiredEffects.send('save-workspace-temp', {data: this.codeEditorState.getCode()});
         });
     }
 }
