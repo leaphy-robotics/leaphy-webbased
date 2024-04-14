82e313f48d419c7ab3cfc0c19afcbc52c8837376
Koen <98043234+koen1711@users.noreply.github.com>
2024-04-14 18:18:54 +0200

    fix: flitz examples (#223)
    * chore: upgrade angular
    
    * chore: migrate to angular v17
    
    * chore: fix eslint
    
    * fix: add back matomo
    
    * chore: update monaco
    
    * fix: flitz examples

--- src/app/modules/core/dialogs/examples/examples-dialog.component.ts
+++ src/app/modules/core/dialogs/examples/examples-dialog.component.ts
@@ -1,6 +1,6 @@
 import {MatDialogRef} from "@angular/material/dialog";
 import {Component} from "@angular/core";
-import examples, {Board, Example} from "src/examples";
+import examples, {Board, BoardNames, BoardNamesArray, Example} from "src/examples";
 import {AppState} from "../../../../state/app.state";
 import {WorkspaceService} from "../../../../services/workspace.service";
 
@@ -23,15 +23,40 @@ export class ExamplesDialog {
     }
 
     private getExamplesForRobot(id: string): Example[] {
-        return examples.filter(example => {
-            if (example.boards.includes(Board.ALL)) return true
-            if (example.boards.includes(Board.L_ORIGINAL_ALL) && id.startsWith('l_original')) return true
-            if (example.boards.includes(Board.L_ORIGINAL_NANO_ALL) && id.startsWith('l_original_nano')) return true
-            if (example.boards.includes(Board.L_FLITZ_ALL) && id.startsWith('l_flitz')) return true
-            if (example.boards.includes(Board.L_NANO_ALL) && id.startsWith('l_nano')) return true
-            if (example.boards.includes(Board.L_ARDUINO) && ['l_nano', 'l_nano_esp32', 'l_nano_rp2040', 'l_uno', 'l_mega'].includes(id)) return true
 
-            return example.boards.map(board => board as string).includes(id)
+        return examples.filter(example => {
+            let allowed = false;
+            if (example.boards.includes(Board.ALL)) allowed = true
+            if (example.boards.includes(Board.L_ORIGINAL_ALL) && id.startsWith('l_original')) allowed = true
+            if (example.boards.includes(Board.L_ORIGINAL_NANO_ALL) && id.startsWith('l_original_nano')) allowed = true
+            if (example.boards.includes(Board.L_FLITZ_ALL) && id.startsWith('l_flitz')) allowed = true
+            if (example.boards.includes(Board.L_NANO_ALL) && id.startsWith('l_nano')) allowed = true
+            if (example.boards.includes(Board.L_ARDUINO) && ['l_nano', 'l_nano_esp32', 'l_nano_rp2040', 'l_uno', 'l_mega'].includes(id)) allowed = true
+
+            // check if there is a negative number in the list
+            // @ts-ignore
+            if (allowed == true) {
+                let negative = [...example.boards].filter(board => board < 0)
+                if (negative.length > 0) {
+                    // @ts-ignore
+                    if (negative.includes(-BoardNames[id])) {
+                        return false
+                    } else {
+                        if (example.boards.includes(-Board.ALL)) return false
+                        if (example.boards.includes(-Board.L_ORIGINAL_ALL) && id.startsWith('l_original')) return false
+                        if (example.boards.includes(-Board.L_ORIGINAL_NANO_ALL) && id.startsWith('l_original_nano')) return false
+                        if (example.boards.includes(-Board.L_FLITZ_ALL) && id.startsWith('l_flitz')) return false
+                        if (example.boards.includes(-Board.L_NANO_ALL) && id.startsWith('l_nano')) return false
+                        return !(example.boards.includes(-Board.L_ARDUINO) && ['l_nano', 'l_nano_esp32', 'l_nano_rp2040', 'l_uno', 'l_mega'].includes(id));
+
+
+                    }
+                } else {
+                    return true
+                }
+            }
+
+            return example.boards.includes(BoardNames[id])
         })
     }
 
--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,25 +1,49 @@
 import {AppState} from "src/app/state/app.state";
 
 export enum Board {
-    ALL = '*',
+    ALL = 1,
 
-    L_ORIGINAL_ALL = 'l_original_*',
-    L_ORIGINAL_UNO = 'l_original_uno',
-    L_ORIGINAL_NANO_ALL = 'l_original_nano_*',
-    L_ORIGINAL_NANO = 'l_original_nano',
+    L_ORIGINAL_ALL,
+    L_ORIGINAL_UNO,
+    L_ORIGINAL_NANO_ALL,
+    L_ORIGINAL_NANO,
 
-    L_FLITZ_ALL = 'l_flitz_*',
-    L_FLITZ_UNO = 'l_flitz_uno',
-    L_FLITZ_NANO = 'l_flitz_nano',
+    L_FLITZ_ALL,
+    L_FLITZ_UNO,
+    L_FLITZ_NANO,
 
-    L_CLICK = 'l_click',
+    L_CLICK,
 
-    L_ARDUINO = 'l_arduino_*',
-    L_UNO = 'l_uno',
-    L_NANO_ALL = 'l_nano_*',
-    L_NANO = 'l_nano',
-    L_MEGA = 'l_mega'
+    L_ARDUINO,
+    L_UNO,
+    L_NANO_ALL,
+    L_NANO,
+    L_MEGA
 }
+
+
+export const BoardNames = {
+    '*': Board.ALL,
+
+    'l_original_*': Board.L_ORIGINAL_ALL,
+    'l_original_uno': Board.L_ORIGINAL_UNO,
+    'l_original_nano_*': Board.L_ORIGINAL_NANO_ALL,
+    'l_original_nano': Board.L_ORIGINAL_NANO,
+
+    'l_flitz_*': Board.L_FLITZ_ALL,
+    'l_flitz_uno': Board.L_FLITZ_UNO,
+    'l_flitz_nano': Board.L_FLITZ_NANO,
+
+    'l_click': Board.L_CLICK,
+
+    'l_arduino_*': Board.L_ARDUINO,
+    'l_uno': Board.L_UNO,
+    'l_nano_*': Board.L_NANO_ALL,
+    'l_nano': Board.L_NANO,
+    'l_mega': Board.L_MEGA
+}
+
+export const BoardNamesArray = Object.values(BoardNames)
 
 export interface Example {
     name: string,
@@ -33,7 +57,7 @@ export default [
         name: 'Blink',
         sketch: 'blink.l_example',
         image: 'blink.svg',
-        boards: [Board.ALL]
+        boards: [Board.ALL, -Board.L_FLITZ_ALL]
     },
     {
         name: 'Snake',
