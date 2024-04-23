2423bb2933321e3c93f5216ddbbee6273e15e92d
sverben <59171289+sverben@users.noreply.github.com>
2024-04-23 10:30:14 +0200

    fix: new robots mapping

--- src/app/domain/robots.ts
+++ src/app/domain/robots.ts
@@ -1,15 +1,5 @@
-import { arduino } from "@leaphy-robotics/leaphy-blocks";
 import Avrdude from "../services/arduino-uploader/protocols/avrdude";
-import {
-    BaseNano,
-    BaseNanoESP32,
-    BaseNanoRP2040,
-    BaseUno,
-    PinMapping,
-    RobotType,
-} from "./robot.type";
-import DFU from "../services/arduino-uploader/protocols/dfu";
-import Pico from "../services/arduino-uploader/protocols/pico";
+import {BaseNano, BaseNanoESP32, BaseNanoRP2040, BaseUno, PinMapping, RobotType,} from "./robot.type";
 
 const defaultLibraries = [
     "Leaphy Extensions",
@@ -214,6 +204,7 @@ export const arduinoUnoRobotTypeGeneric = new BaseUno(
 export const arduinoMegaRobotTypeGeneric = new RobotType(
     "l_mega",
     { protocol: Avrdude, microcontroller: "atmega2560" },
+    PinMapping.MEGA,
     "Arduino Mega",
     "mega.svg",
     null,
@@ -241,6 +232,7 @@ export const arduinoNanoESP32RobotTypeGeneric = new BaseNanoESP32(
 export const arduinoNanoEveryRobotTypeGeneric = new RobotType(
     "l_nano_every",
     { protocol: Avrdude, microcontroller: "megaavr" },
+    PinMapping.NANO,
     "Arduino Nano Every",
     "nano.svg",
     null,
